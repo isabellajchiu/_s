5f2fe0d99d85654c5859717f945863fe69b26946
isabellajchiu <isabellajchiu@gmail.com>
2016-12-31 17:53:12 -0500

    update header font

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Dancing+Script' rel='stylesheet' type='text/css'>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
